99827e68246e41efc823623902df5c41894d08a9
Harold Restrepo <harold.r.restrepo@hotmail.com>
2014-08-10 17:36:41 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,26 @@
+## Downloading data
+url<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destino <- "household_power_consumption.zip"
+
+download.file(url,destino)
+
+sourcefile <- unz(destino, "household_power_consumption.txt")
+
+data <- read.table(sourcefile, header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+## Subsetting the data
+data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
+
+## Converting dates
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+## Loading date in memory
+DataFile <- "data.csv"
+
+write.csv(data, DataFile)
+
+## Plot 4
+
 png(filename = "plot4.png", width = 480, height = 480, units = "px", bg="transparent")
 
 par (mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
