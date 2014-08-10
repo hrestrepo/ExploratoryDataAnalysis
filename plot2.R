ae9851b5ba96bf97398102b993a1b1c38da8f9ac
Harold Restrepo <harold.r.restrepo@hotmail.com>
2014-08-10 17:37:53 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -19,7 +19,7 @@ DataFile <- "data.csv"
 
 write.csv(data, DataFile)
 
-## Plot 1
+## Plot 2
 par(mfrow=c(1,1))
 png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
 plot(x = data$DateTime, y = data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
