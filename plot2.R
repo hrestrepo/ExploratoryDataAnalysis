49f5b7a3ade9ee4748ab2f725080920886fc385c
Harold Restrepo <harold.r.restrepo@hotmail.com>
2014-08-10 17:35:14 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,25 @@
+## Downloading data
+url<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destino <- "household_power_consumption.zip"
 
+download.file(url,destino)
+
+sourcefile <- unz(destino, "household_power_consumption.txt")
+
+data <- read.table(sourcefile, header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+## Subsetting the data
+data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
+
+## Converting dates
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+## Loading date in memory
+DataFile <- "data.csv"
+
+write.csv(data, DataFile)
+
+## Plot 1
 par(mfrow=c(1,1))
 png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
 plot(x = data$DateTime, y = data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
